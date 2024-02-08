33942f315e769ac0b0a0281c435dcc1f2797c1af
Berkcan <berkcan.0942@gmail.com>
2024-02-09 00:19:58 +0300

    My first git commit

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Intro.Business;
+using Intro.Entities;
+
+string message1 = "My name is Berkcan";
+
+int age = 21;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine(message1);
+Console.WriteLine("I'm " + age + " years old!");
+
+CourseManager courseManager = new CourseManager();
+
+Course[] courses2 = courseManager.GetAll();
+
+for (int i = 0;i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+
+Console.WriteLine("Kod bitti."); 
